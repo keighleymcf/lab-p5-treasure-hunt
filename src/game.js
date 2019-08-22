af3537ff72a85ab10ba714ffaf3a397b522d5c89
Saga Åkermark <sagaakermark@hotmail.com>
2019-08-22 17:03:33 +0200

    done

--- src/game.js
+++ src/game.js
@@ -3,5 +3,46 @@ class Game {
     // Iteration 1
     // Draw the grid
     // https://p5js.org/reference/#/p5/line
+    for (let i = 0; i <= 10; i++) {
+      line(100 * i, 0, 100 * i, HEIGHT);
+      line(0, SQUARE_SIDE * i, WIDTH, SQUARE_SIDE * i)
+    }
+  }
+}
+
+class Player {
+  constructor(col, row) {
+    this.col = col;
+    this.row = row;
+  }
+  moveUp() {
+    this.col -= 1;
+  }
+
+  moveDown() {
+    this.col += 1;
+  }
+  moveLeft() {
+    this.row -= 1;
+  }
+  moveRight() {
+    this.row += 1;
+  }
+  draw() {
+    image(img, this.row * SQUARE_SIDE, this.col * SQUARE_SIDE, 100, 100);
+  }
+}
+
+class Treasure {
+  constructor(col, row) {
+    this.col = col;
+    this.row = row;
+  }
+  setRandomPosition() {
+    this.col = Math.random() * 10;
+    this.row = Math.random() * 10;
+  }
+  drawTreasure() {
+    image(treasure, this.row * SQUARE_SIDE, this.col * SQUARE_SIDE, 100, 100);
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,34 @@
 const game = new Game();
+const player = new Player(0, 1);
+let img;
+let treasure;
+const treasureBox = new Treasure(0, 0);
+
 
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
   canvas.parent("canvas");
+  img = loadImage(`assets/character-down.png`)
+  treasureBox.setRandomPosition();
+  treasure = loadImage(`assets/treasure.png`)
+
 }
 
 function draw() {
+  clear();
   game.drawGrid();
+  player.draw();
+  treasureBox.drawTreasure();
+}
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    player.moveUp();
+  } else if (keyCode === DOWN_ARROW) {
+    player.moveDown();
+  } else if (keyCode === RIGHT_ARROW) {
+    player.moveRight();
+  } else if (keyCode === LEFT_ARROW) {
+    player.moveLeft();
+  }
 }
